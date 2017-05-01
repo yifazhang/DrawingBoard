b3a68d026b83e9acd334f935ec36593680ad9ccb
yifazhang <grant2015@126.com>
2017-05-01 23:02:26 +0800

    add gray Color

--- DrawingBoard/src/main/java/com/zhangyifa/DrawingBoard/ui/ColorPanel.java
+++ DrawingBoard/src/main/java/com/zhangyifa/DrawingBoard/ui/ColorPanel.java
@@ -26,14 +26,14 @@ public class ColorPanel extends Panel implements ActionListener{
 	private void initColor() {
 		listColor.add(Color.BLACK);
 		listColor.add(Color.BLUE);
-		listColor.add(Color.BLUE);
+		listColor.add(Color.GRAY);
 		
 		listColor.add(Color.DARK_GRAY);
 		listColor.add(Color.LIGHT_GRAY);
 		listColor.add(Color.GREEN);
 
 		listColor.add(Color.CYAN);
-		listColor.add(Color.BLUE);
+		listColor.add(Color.MAGENTA);
 		listColor.add(Color.PINK);
 
 		listColor.add(Color.RED);
